a66c71cf23c982738f4571f5c631163286fe1342
Christian Duarte <w.christian.duarte.@outlook.com>
2023-09-20 00:41:24 -0700

    Removed unused import

--- src/about-me.ts
+++ src/about-me.ts
@@ -1,5 +1,5 @@
 import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement } from 'lit/decorators.js'
 
 @customElement('cd-about-me')
 export class AboutMe extends LitElement {
